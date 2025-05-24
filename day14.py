cc23f9a2b915500fc4e788b3341f62745d28d533
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2025-05-25 00:34:21 +0600

    allhamdulliha for today

--- day14.py
+++ day14.py
@@ -13,18 +13,28 @@
 
 
 
-num = int(input("Enter the value of num: "))
+# num = int(input("Enter the value of num: "))
+# if(num<0):
+#     print("Number is negative")
+# elif(num==0):
+#     print("Number is Zero")
+# else:
+#     print("Number is Positive.")
+    
+# print("I am happy now")
+
+num = int(input("Enter the number: "))
 if(num<0):
     print("Number is negative")
-elif(num==0):
-    print("Number is Zero")
+elif(num > 0):
+    if(num<=10):
+        print("Number is between 1-10")
+    elif(num > 10 and num <=20):
+        print("Number is between 11 - 20") 
+    else: 
+        print("Number is greater than 20")
 else:
-    print("Number is Positive.")
-    
-print("I am happy now")
-
-
-
+    print("Number is zero")
 
 
 
