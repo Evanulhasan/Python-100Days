128e738f4faf06369fb8e2cb40a6df7d2dda6e6f
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2025-05-27 03:47:27 +0600

    Allhamdulliha

--- day18.py
+++ day18.py
@@ -9,14 +9,37 @@
 # print("Done with the loop")
 
 # Decrementing operation 
-count = 5 
-while(count>0):
-    print(count)
-    count = count-1
+# count = 5 
+# while(count>0):
+#     print(count)
+#     count = count-1
+# else: 
+#     print("I am inside else")
 
+#Break Statement
 
+# i = 1 
+# while i < 6: 
+#     print(i)
+#     if i == 3:
+#         break 
+#     i+= 1
 
+# # Continue Statement 
+# i = 0 
+# while i < 6: 
+#     i += 1 
+#     if i == 3:
+#         continue 
+#     print(i)
 
+# else Statement  
+i = 1 
+while i < 6: 
+    print(i)
+    i += 1 
+else: 
+    print("i is no longer less than 6")
 
 
 
