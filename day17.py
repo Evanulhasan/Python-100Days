61cbce82a7add264fe1219d92711d5f83aa4512f
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2025-05-27 03:37:24 +0600

    Allhamdulliha

---
+++ day17.py
@@ -0,0 +1,40 @@
+# For loops in Python 
+
+# name = "Evanul Hasan Oualid"
+# for i in name: 
+#     print(i, end=",")
+#     if(i == "a"):
+#         print("This is something special!")
+
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# for a in range(1,40,3):
+#     print(a)
+
+# for char in "Python":
+#     print(char)
+
+# num = int(input("Enter the number: "))
+
+# for i in range(10):
+#     print(f"{i+1} * num = {(i+1)*num}")
+    
+
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{j} * {i} = {i*j}")
+#     print("\n")
+
+
+
+
+
+
+
+
+
---
+++ day18.py
@@ -0,0 +1,36 @@
+# while loops in Python 
+
+# i = int(input("Enter the number: "))
+# while(i<=38):
+#     i = int(input("Enter the number: "))
+#     print(i)
+#     # i = i + 1
+    
+# print("Done with the loop")
+
+# Decrementing operation 
+count = 5 
+while(count>0):
+    print(count)
+    count = count-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
