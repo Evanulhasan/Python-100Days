4f457df3edcf5d6fe539efa84788767f80647c6c
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2025-05-28 20:39:27 +0600

    Start

---
+++ day19.py
@@ -0,0 +1,48 @@
+# Break and Continue in Python 
+
+#Break
+# for i in range(12):
+#     if(i == 10):
+#         break
+#     print("5 X", i+1, "=", 5* (i+1))
+
+# print("if i == 10 break the loop.")
+
+#Continue 
+# for i in range(1,12):
+#     if(i == 10): 
+#         print("Skip the iteration")
+#         continue
+#     print("5 X", i, "=", 5 * (i))
+    
+
+# In Python do while loop not exist thats why we use infinet while loop in python 
+# To make do while loop 
+i = 1
+while True:
+    print(i)
+    i = i+1
+    if(i%1==0):
+        break
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
